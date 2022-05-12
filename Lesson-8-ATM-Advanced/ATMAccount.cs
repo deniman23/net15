65aa37aee910a139890d9b5b25bf3f32b5bc17ef
danetolk <94922898+deniman23@users.noreply.github.com>
2022-05-12 12:35:09 +0300

    Add exception for "next"

---
+++ Lesson-8-ATM-Advanced/ATMAccount.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ATMAccount
+{
+    private const int MaxAmount = 100;
+    public List<Operation> History { get; } = new List<Operation> ();
+    public string CardNumber { get; }
+    public int Ballance { get; private set; }
+    public ATMAccount(string cardNumber)
+    {
+        CardNumber = cardNumber;
+        Ballance = 100;
+    }
+    public void WithdrawMoney(int amount)
+    {
+        var operation = new Operation();
+        try
+        {
+            if (Ballance < amount)
+            {
+                throw new InvalidOperationException("Недостаточно средств");
+            }
+            Ballance -= amount;
+            operation.Result = OperationResult.Success;
+        }
+        catch (Exception)
+        {
+            operation.Result = OperationResult.Failure;
+            throw;
+        }
+        finally
+        {
+            operation.Type = OperationType.Withdraw;
+            operation.Balance = Ballance;
+            History.Add(operation);
+        }
+    }
+    public void AddMoney(int amount)
+    {
+        var operation = new Operation();
+       
+        try
+        {
+            if (amount > MaxAmount)
+            {
+                throw new InvalidOperationException("Превышен лимит пополнения");
+            }
+
+            Ballance += amount;
+            operation.Result = OperationResult.Success;
+        }
+        catch (Exception)
+        {
+            operation.Result = OperationResult.Failure;
+            throw;
+        }
+        finally
+        {
+            operation.Type = OperationType.Withdraw;
+            operation.Balance = Ballance;
+            History.Add(operation);
+        }
+    }
+}
+public class Operation
+{
+    public OperationType Type { get; set; }
+    public int Balance { get; set; }
+    public OperationResult Result { get; set; }
+    public override string ToString()
+    {
+        return string.Format(
+            "Балланс после операции: {0}, Тип операции: {1}, Результат: {2}",
+            Balance, Type, Result);
+    }
+}
+
+public enum OperationType
+{
+    Add,
+    Withdraw
+}
+public enum OperationResult
+{
+    Success,
+    Failure
+}
---
+++ Lesson-8-ATM-Advanced/ATMClient.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using static HistoryViewingEventArgs;
+
+/// <summary>
+/// Класс для эмуляции поведения пользователя банкомата.
+/// </summary>
+public class ATMClient
+{
+    private readonly Random _random;
+    private readonly Action[] _actions;
+
+    private ATMAccount _account;
+
+    public ATMClient()
+    {
+        _random = new Random();
+        _actions = new Action[] { ViewAccount, WithdrawMoney, InsertMoney };
+    }
+
+    public Action<string, int> modifyingAccount;
+
+    public event Action<string> CardInserted;
+    public event Action<int> ViewingAccount;
+    public event Action<HistoryViewingEventArgs> ViewingHistory;
+    public event Action<AccountModifyingEventArgs> ModifyingAccount;
+    public event Action<int> ModifiedAccount;
+    public event Action<string> InvalidOperation;
+
+    public void DoRandomActions()
+    {
+        InsertCard();
+
+        var numberOfActions = _random.Next(10, 15);
+        for (int i = 0; i < numberOfActions; i++)
+        {
+            _actions[_random.Next(3)]();
+        }
+
+        ViewHistory();
+    }
+
+    private void InsertCard()
+    {
+        _account = new ATMAccount(Guid.NewGuid().ToString());
+
+        CardInserted?.Invoke(_account.CardNumber);
+    }
+
+    private void ViewAccount()
+    {
+        ViewingAccount?.Invoke(_account.Ballance);
+    }
+
+    private void WithdrawMoney()
+    {
+        try
+        {
+            _account.WithdrawMoney(_random.Next(100));
+        }
+        catch(InvalidOperationException ex)
+        {
+            InvalidOperation?.Invoke(ex.Message);
+        }
+
+        ModifiedAccount?.Invoke(_account.Ballance);
+    }
+
+    private void InsertMoney()
+    {
+        if (ModifyingAccount != null)
+        {
+            try
+            {
+                var eventArgs = new AccountModifyingEventArgs(_account.CardNumber);
+                
+                ModifyingAccount.Invoke(eventArgs);
+
+                _account.AddMoney(eventArgs.Amount);
+
+                ModifiedAccount?.Invoke(_account.Ballance);
+            }
+            catch (InvalidOperationException ex)
+            {
+                InvalidOperation?.Invoke(ex.Message);
+            }
+        }
+    }
+
+    private void ViewHistory()
+    {
+        if (ViewingHistory != null)
+        {
+            var accountHistory = new AccountHistoryStateMachine(this._account);
+            try
+            {
+                while (accountHistory.NextOperation())
+                {
+                    ViewingHistory.Invoke(accountHistory.CurrentOperationArgs);
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                InvalidOperation?.Invoke(ex.Message);
+            }
+
+        }
+    }
+
+    private class AccountHistoryStateMachine
+    {
+        private const int MaxHistoryOperations = 2;
+
+        private readonly ATMAccount _account;
+
+        private HistoryViewingEventArgs.HistoryOperation _currentOperation = default;
+        private int _currentOperationListPageIndex = -1;
+
+        public AccountHistoryStateMachine(ATMAccount account)
+        {
+            this._account = account;
+        }
+
+        internal HistoryViewingEventArgs CurrentOperationArgs { get; private set; }
+
+        internal bool NextOperation()
+        {
+            if (CurrentOperationArgs != null)
+            {
+                _currentOperation = CurrentOperationArgs.NextOperation;
+            }
+
+            if (_currentOperation == HistoryViewingEventArgs.HistoryOperation.Quit)
+            {
+                _currentOperationListPageIndex = -1;
+                return false;
+            }
+
+            if (_currentOperation == HistoryViewingEventArgs.HistoryOperation.NotSpecified)
+            {
+                CurrentOperationArgs = new HistoryViewingEventArgs
+                {
+                    AllowedOperations = new[] 
+                    {
+                        HistoryViewingEventArgs.HistoryOperation.TransactionHistory,
+                        HistoryViewingEventArgs.HistoryOperation.Quit
+                    }
+                };
+            }
+
+            if (_currentOperation == HistoryViewingEventArgs.HistoryOperation.TransactionHistory)
+            {
+                var data = "Всего оппераций " + this._account.History.Count.ToString();
+
+                var allowedOperations = new List<HistoryOperation>
+                    {
+                        HistoryViewingEventArgs.HistoryOperation.GoBack,
+                        HistoryViewingEventArgs.HistoryOperation.Quit
+                    };
+
+                if (this._account.History.Count > MaxHistoryOperations)
+                {
+                    allowedOperations.Insert(0, HistoryViewingEventArgs.HistoryOperation.NextTransactions);
+                    _currentOperationListPageIndex = 1;
+                }
+
+                #region UI flow
+                // [1,2,3]
+
+                // list -> операция 1
+                // next -> операция 2
+                // next -> операция 3
+                // back, quit
+                #endregion
+
+                for (int i = 0; i < MaxHistoryOperations; i++)
+                {
+                    if (i < _account.History.Count)
+                    {
+                        var operation = _account.History[i];
+                        data += "\nОперация номер " + (i + 1).ToString() + "\t\n" + operation.ToString();
+                    }
+                    
+                }
+                CurrentOperationArgs.Data = data;
+                CurrentOperationArgs.AllowedOperations = allowedOperations.ToArray();
+            }
+
+            if (_currentOperation == HistoryViewingEventArgs.HistoryOperation.NextTransactions)
+            {
+                var allowedOperations = new List<HistoryOperation>
+                {
+                    HistoryViewingEventArgs.HistoryOperation.GoBack,
+                    HistoryViewingEventArgs.HistoryOperation.Quit
+                };
+
+                #region Paging example
+                // items per page: 2 (Max)
+                // page number
+                //  [1,2, 3,4, 5] -> pageCount -> 5 % 2 = 1
+                //i: 0,1  2,3, 4
+
+                // 0:Max -> page 0 -> (pageIndex * Max: (pageIndex + 1) * Max - 1) -> (0 : 1)
+                // 2:3   -> page 1 -> (pageIndex * Max: ???) -> (2 : 3)
+                // 4     -> page 2 -> (pageIndex * Max: ???) -> (4 : 5)
+                #endregion
+
+                int pageIndex = this._currentOperationListPageIndex;
+
+                int pageCount = _account.History.Count / MaxHistoryOperations; // 5 / 2 = 2
+                
+                if (_account.History.Count % MaxHistoryOperations > 0)
+                {
+                    pageCount++;
+                }
+
+                #region Operator examples
+                // i *= 2 -> i = i * 2
+
+                // i = 0;
+                // var j = i++; // j = 0; i = 1;
+                // var j = ++i; // j =  1; i = 1;
+
+                // page count: 2
+                // [1,2] 
+                // page 0 -> [0:0]
+                // page 1 -> [1:1]
+                #endregion
+
+                var data = "Страница " + (pageIndex + 1) + ". Всего страниц " + pageCount;
+                for (int i = pageIndex * MaxHistoryOperations; i <= (pageIndex + 1) * MaxHistoryOperations - 1; i++)
+                {
+                        if (i < _account.History.Count)
+                        {
+                            var operation = _account.History[i];
+                            data += "\nОперация номер " + (i + 1).ToString() + "\t\n" + operation.ToString();
+                        
+                        }
+                    else
+                    {
+                        throw new Exception("Превышен лимит действий");
+                    }
+                    
+                }
+                this._currentOperationListPageIndex++;
+
+                if (pageIndex < pageCount - 1)
+                {
+                    allowedOperations.Insert(0, HistoryViewingEventArgs.HistoryOperation.NextTransactions);
+                }
+
+                CurrentOperationArgs.Data = data;
+                CurrentOperationArgs.AllowedOperations = allowedOperations.ToArray();
+
+            }
+
+            if (_currentOperation == HistoryViewingEventArgs.HistoryOperation.GoBack)
+            {
+
+            }
+
+            CurrentOperationArgs.CurrentOperation = _currentOperation;
+
+            return true;
+        }
+    }
+}
+
+public class AccountModifyingEventArgs
+{
+    public AccountModifyingEventArgs(string cardNumber)
+    {
+        CardNumber = cardNumber;
+    }
+
+    public int Amount { get; set; }
+    public string CardNumber { get; }
+}
+
+public class HistoryViewingEventArgs
+{
+    public string Data { get; internal set; }
+    public HistoryOperation[] AllowedOperations { get; internal set; }
+    public HistoryOperation CurrentOperation { get; internal set; }
+    public HistoryOperation NextOperation { get; set; }
+
+    public enum HistoryOperation
+    {
+        NotSpecified,
+        TransactionHistory,
+        NextTransactions,
+        GoBack,
+        Quit
+    }
+}
